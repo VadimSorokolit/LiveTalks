41413143b8ffdeb775aa9a9aee53f0ce10d8be52
VadimSorokolit <macintosh@ukr.net>
2025-07-21 08:38:16 +0300

    init project

---
+++ LiveTalks/LiveTalks/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  LiveTalks
+//
+//  Created by Vadim Sorokolit on 21.07.2025.
+//
+    
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
