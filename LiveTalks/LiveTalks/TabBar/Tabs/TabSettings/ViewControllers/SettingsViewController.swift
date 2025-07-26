e5ad826ae0a25423036c973b6584aa792abdcaed
VadimSorokolit <macintosh@ukr.net>
2025-07-26 16:07:07 +0300

    Implemented share app button

--- LiveTalks/LiveTalks/TabBar/Tabs/TabSettings/ViewControllers/SettingsViewController.swift
+++ LiveTalks/LiveTalks/TabBar/Tabs/TabSettings/ViewControllers/SettingsViewController.swift
@@ -13,6 +13,17 @@ class SettingsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let shareImage = UIImage(systemName: "square.and.arrow.up")
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: shareImage, style: .plain, target: self, action: #selector(self.didTapShareAppButton))
+    }
+    
+    @objc private func didTapShareAppButton() {
+        let text = "Check out this app!"
+        let url  = URL(string: "https://apps.apple.com/app/id88")!
+
+        let viewController = UIActivityViewController(activityItems: [text, url], applicationActivities: nil)
+        present(viewController, animated: true)
     }
     
 }
